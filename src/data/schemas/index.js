fdac79dee3bd6c1cf6fb1425a3801de6af7350b0
subin <cdw3521@naver.com>
2024-06-06 16:53:19 +0900

    schema export

---
+++ src/data/schemas/index.js
@@ -0,0 +1,5 @@
+const userSchema = require('./userSchema');
+const productSchema = require('./productSchema');
+const guestSchema = require('./guestSchema');
+
+module.exports = { userSchema, productSchema, guestSchema };
