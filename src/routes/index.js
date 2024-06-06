cc3bebc4695fc5c27a5ab54e69b7677f335fd4db
subin <cdw3521@naver.com>
2024-06-06 16:54:35 +0900

    라우터 export

---
+++ src/routes/index.js
@@ -0,0 +1,4 @@
+const signupRouter = require('./signupRouter');
+const homeRouter = require('./homeRouter');
+const productRouter = require('./productRouter');
+module.exports = { signupRouter, homeRouter, productRouter };
