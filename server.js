892feca4bfe3660ca900ca8ca61a7150bba2b134
subin <cdw3521@naver.com>
2024-06-07 14:53:10 +0900

    server.js 파일 위치 변경

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config({ path: './.env' });
+const express = require('express');
+const mongoose = require('mongoose');
+
+const router = require('./src/app');
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+    console.log('DB와 연결되었습니다😄');
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('DB와 연결이 끊어졌습니다🥲');
+});
+
+mongoose.connection.on('error', () => {
+    console.log('⚠️ DB와 연결 과정에서 오류가 발생하였습니다.');
+    console.log(process.env.MONGODB_URI);
+});
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api', router);
+
+app.listen(7008, () => {
+    console.log('PORT 7008과 연결되었습니다😊');
+});
