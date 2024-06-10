6b756d88d89bb0f92da966d1a633313255bda73d
subin <cdw3521@naver.com>
2024-06-10 17:53:24 +0900

    Docs: 대분류 카테고리별 상품 조회하는 코드 수정

--- src/routes/categoryRouter.js
+++ src/routes/categoryRouter.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const Category = require('../data/models/Category');
+const { subCategory, Category, Product } = require('../data');
 
-// 대분류 카테고리 조회
+// 대분류 카테고리별 상품 조회
 router.get('/:categoryNumber', async (req, res, next) => {
     const { categoryNumber } = req.params;
     if (!Number.isInteger(categoryNumber)) {
@@ -11,19 +11,21 @@ router.get('/:categoryNumber', async (req, res, next) => {
         next(err);
         return;
     }
-    try {
-        const category = await Category.find({ categoryNumber: Number(categoryNumber) }).lean();
 
-        if (!category) {
-            const err = new Error('해당 카테고리를 찾을 수 없습니다.');
+    try {
+        const categoryProduct = await Product.find({ categoryNumber: Number(categoryNumber) }).lean();
+
+        if (!categoryProduct) {
+            const err = new Error('해당 카테고리의 상품을 찾을 수 없습니다.');
             err.statusCode = 404;
             next(err);
             return; // 매우 중요.  return 해주지 않을 경우 response가 간 다음에도 이후 코드들이 실행됨
         }
 
         return res.json({
-            categoryName: category.categoryName,
-            categoryNumber: category.categoryNumber,
+            image: categoryProduct.image,
+            name: categoryProduct.name,
+            price: categoryProduct.price,
         });
     } catch (e) {
         next(e);
