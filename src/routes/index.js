60858dfc5e6c23002e1b381a42e6c84bb8ed9123
subin <cdw3521@naver.com>
2024-06-11 11:52:59 +0900

    Docs: 라우터 및 미들웨어 코드 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,15 @@ const authRouter = require('./authRouter');
 const adminCategoryRouter = require('./adminCategoryRouter');
 const categoryRouter = require('./categoryRouter');
 const userCrudRouter = require('./userCrudRouter');
-const { cookiesMiddleware, isAuthenticatedMiddleware, isAuthenticatedAdminMiddleware } = require('../middlewares');
+const productRouter = require('./productRouter');
+const { isAuthenticatedMiddleware, isAuthenticatedAdminMiddleware } = require('../middlewares');
+const detailProductRouter = require('./detailProductRouter');
 
-apiRouter.use('/', cookiesMiddleware, authRouter); // 회원가입 및 로그인 라우터 연결
-apiRouter.use('/categories', cookiesMiddleware, categoryRouter); // 카테고리 조회 및 카테고리별 상품 조회하는 라우터 연결
-apiRouter.use('/admin/categories', cookiesMiddleware, isAuthenticatedAdminMiddleware, adminCategoryRouter); // 카테고리 수정 & 삭제 & 추가 라우터 연결
-apiRouter.use('/my-page', cookiesMiddleware, isAuthenticatedMiddleware, userCrudRouter); // 회원 정보 조회 & 수정 & 삭제 라우터 연결
+apiRouter.use('/', authRouter); // 회원가입 및 로그인 라우터 연결
+apiRouter.use('/categories', categoryRouter); // 카테고리 조회 및 카테고리별 상품 조회하는 라우터 연결
+apiRouter.use('/admin/categories', isAuthenticatedAdminMiddleware, adminCategoryRouter); // 카테고리 수정 & 삭제 & 추가 라우터 연결
+apiRouter.use('/my-page', isAuthenticatedMiddleware, userCrudRouter); // 회원 정보 조회 & 수정 & 삭제 라우터 연결
+apiRouter.use('/admin/products', productRouter);
+apiRouter.use('/products', detailProductRouter);
 
 module.exports = apiRouter;
