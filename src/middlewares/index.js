ba1b4abe9d20676aa08d2abb087e7419ae9972db
subin <cdw3521@naver.com>
2024-06-11 11:47:47 +0900

    Remove: 미들웨어 삭제

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,5 +1,4 @@
-const cookiesMiddleware = require('./cookiesMiddleware');
 const isAuthenticatedAdminMiddleware = require('./isAuthenticatedAdmin');
 const isAuthenticatedMiddleware = require('./isAuthenticated');
 
-module.exports = { cookiesMiddleware, isAuthenticatedAdminMiddleware, isAuthenticatedMiddleware };
+module.exports = { isAuthenticatedAdminMiddleware, isAuthenticatedMiddleware };
