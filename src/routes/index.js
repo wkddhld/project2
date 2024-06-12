c0fc00b2dcfd482b87e20608e0d6058ab7ac6397
subin <cdw3521@naver.com>
2024-06-12 21:23:31 +0900

    Docs: 주문 Crud 기능 수행하는 라우터 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ const userCrudRouter = require('./userCrudRouter');
 const productRouter = require('./productRouter');
 const { isAuthenticatedMiddleware, isAuthenticatedAdminMiddleware } = require('../middlewares');
 const detailProductRouter = require('./detailProductRouter');
+const checkOrderRouter = require('./checkOrderRouter');
 
 apiRouter.use('/', authRouter); // 회원가입 및 로그인 라우터 연결
 apiRouter.use('/categories', categoryRouter); // 카테고리 조회 및 카테고리별 상품 조회하는 라우터 연결
@@ -13,5 +14,6 @@ apiRouter.use('/admin/categories', isAuthenticatedAdminMiddleware, adminCategory
 apiRouter.use('/my-page', isAuthenticatedMiddleware, userCrudRouter); // 회원 정보 조회 & 수정 & 삭제 라우터 연결
 apiRouter.use('/admin/products', productRouter);
 apiRouter.use('/products', detailProductRouter);
+apiRouter.use('/orders', checkOrderRouter);
 
 module.exports = apiRouter;
