3f9971281d8dadc801fbf3656cfd4abc911b28f0
subin <cdw3521@naver.com>
2024-06-13 23:24:33 +0900

    Docs : console 코드 삭제

--- src/routes/subCategoryRouter.js
+++ src/routes/subCategoryRouter.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { subCategory,Product } = require('../data'); 
-
+const { subCategory, Product } = require('../data');
 
 // 소분류 카테고리 조회
 router.get('/:categoryNum/:subcategoryNum', async (req, res, next) => {
@@ -9,12 +8,7 @@ router.get('/:categoryNum/:subcategoryNum', async (req, res, next) => {
 
     const categoryNumInt = Number(categoryNum);
     const subcategoryNumInt = Number(subcategoryNum);
-  
-    console.log(`Received categoryNum: ${categoryNum}, subcategoryNum: ${subcategoryNum}`);
-    console.log(`Parsed categoryNumInt: ${categoryNumInt}, subcategoryNumInt: ${subcategoryNumInt}`);
-    console.log(`Is categoryNumInt integer: ${Number.isInteger(categoryNumInt)}, Is subcategoryNumInt integer: ${Number.isInteger(subcategoryNumInt)}`);
- 
- 
+
     if (!Number.isInteger(Number(categoryNum)) || !Number.isInteger(Number(subcategoryNum))) {
         const err = new Error('categoryNumber field는 number type이어야 합니다.');
         err.statusCode = 400;
@@ -23,10 +17,12 @@ router.get('/:categoryNum/:subcategoryNum', async (req, res, next) => {
     }
     try {
         // categoryNum과 scategoryNum에 해당하는 소분류 카테고리 조회
-        const subCate = await subCategory.findOne({ 
-          mainCategoryNumber: categoryNumInt,
-          number: subcategoryNumInt
-        }).lean();
+        const subCate = await subCategory
+            .findOne({
+                mainCategoryNumber: categoryNumInt,
+                number: subcategoryNumInt,
+            })
+            .lean();
 
         if (!subCate) {
             const err = new Error('해당 소분류 카테고리를 찾을 수 없습니다.');
@@ -35,18 +31,19 @@ router.get('/:categoryNum/:subcategoryNum', async (req, res, next) => {
         }
         const products = await Product.find({
             categoryNumber: categoryNumInt,
-            subCateogryNumber: subcategoryNumInt
-        }).lean()
+            subCateogryNumber: subcategoryNumInt,
+        }).lean();
 
         return res.json({
+            err: null,
             data: {
                 subcategory_name: subCate.name,
-                products: products.map(prod => ({
+                products: products.map((prod) => ({
                     image: prod.image,
                     name: prod.name,
-                    price: prod.price
-                }))
-            }
+                    price: prod.price,
+                })),
+            },
         });
     } catch (e) {
         next(e);
