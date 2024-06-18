24a6c5fa58fdee5b98168d606ea4bdfb80b8d8f9
subin <cdw3521@naver.com>
2024-06-19 05:25:00 +0900

    Docs: 코드 수정

--- src/data/schemas/productSchema.js
+++ src/data/schemas/productSchema.js
@@ -16,7 +16,7 @@ const productSchema = new Schema(
         },
         stock: {
             type: Number,
-            required: true,
+            required: false,
         },
         information: {
             type: String,
