b72a3a98cfd47063dcad5070a3aa26b7f2b145a5
[IoT3] 조수빈 <cdw3521@naver.com>
2024-06-27 15:59:24 +0900

    Docs: 코드 리뷰 바탕으로 코드 수정 - 주문 상태 변수로 선언 -update 확인 코드 추가

--- src/routes/adminOrderRouter.js
+++ src/routes/adminOrderRouter.js
@@ -23,6 +23,9 @@ router.put('/:orderNumber', async (req, res, next) => {
     const { orderNumber } = req.params;
     const { orderState } = req.body;
 
+    const orderCancel = '주문취소';
+    const deliveryCompleted = '배송완료';
+
     // 수정하려는 주문이 존재하지 않는 경우
     const order = await Order.findOne({ number: Number(orderNumber) }).lean();
     if (order === null || order === undefined) {
@@ -32,14 +35,15 @@ router.put('/:orderNumber', async (req, res, next) => {
     }
 
     // 수정하려는 주문 상태가 주문취소나 배송완료가 아닌 경우
-    if (!(orderState === '주문취소' || orderState === '배송완료')) {
+    if (!(orderState === orderCancel || orderState === deliveryCompleted)) {
       const err = new Error('올바른 주문 상태가 아닙니다.');
       err.statusCode = 400;
       return next(err);
     }
+    s;
 
     // 주문상태가 "배송완료"인 경우
-    if (order.orderState === '배송완료') {
+    if (order.orderState === deliveryCompleted) {
       const err = new Error('배송 완료 상태이므로 수정이 불가합니다.');
       err.statusCode = 400;
       return next(err);
@@ -47,8 +51,8 @@ router.put('/:orderNumber', async (req, res, next) => {
 
     // 주문이 취소된 상태인 경우
     if (
-      order.orderState === '주문취소' ||
-      (order.orderState === '주문취소' && orderState === '주문취소')
+      order.orderState === orderCancel ||
+      (order.orderState === orderCancel && orderState === orderCancel)
     ) {
       const err = new Error('이미 취소된 주문입니다.');
       err.statusCode = 400;
@@ -56,9 +60,20 @@ router.put('/:orderNumber', async (req, res, next) => {
     }
 
     // 비회원의 주문을 배송완료로 변경 시 user 정보 db에서 삭제
-    await Order.updateOne({ number: Number(orderNumber) }, { orderState }, { new: true });
+    const updateData = await Order.updateOne(
+      { number: Number(orderNumber) },
+      { orderState },
+      { new: true },
+    );
 
-    res.status(201).json({
+    // 업데이트가 정상적으로 되었는지 확인해주는 코드
+    if (updateData.modifiedCount === 0) {
+      const err = new Error('주문 정보를 수정하는 과정에서 오류가 발생했습니다.');
+      err.statusCode = 400;
+      return next(err);
+    }
+
+    res.json({
       err: null,
       data: { orderState, message: '주문 상태가 정상적으로 변경되었습니다.' },
     });
