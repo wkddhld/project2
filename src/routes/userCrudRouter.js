105e2f4ea55e0b1d518df43493ba0481e368fe55
[IoT3] 조수빈 <cdw3521@naver.com>
2024-06-27 16:48:33 +0900

    Docs: 코드 리뷰 바탕으로 코드 수정 - 임시 토큰 발급하는 코드 제거 후 updateLock 필드를 사용하는 코드로 수정 - update 확인하는 코드 추가

--- src/routes/userCrudRouter.js
+++ src/routes/userCrudRouter.js
@@ -1,34 +1,13 @@
 const express = require('express');
-
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-
 const { User } = require('../data');
-
-// 토큰확인 함수
-const verifyToken = (token, secretKey) => {
-  try {
-    return jwt.verify(token, secretKey);
-  } catch (e) {
-    if (e.name === 'TokenExpiredError') {
-      const err = new Error('토큰이 만료되었습니다. 다시 로그인 해주세요.');
-      err.statusCode = 401;
-      throw err;
-    }
-    if (e.name === 'JsonWebTokenError') {
-      const err = new Error('유효하지 않거나 손상된 토큰입니다. 다시 로그인 해주세요.');
-      err.statusCode = 401;
-      throw err;
-    }
-    throw e; // 다른 예기치 않은 에러
-  }
-};
 
 // 사용자 정보 조회
 router.get('/', async (req, res, next) => {
   try {
-    const { adminCookies, tempCookies } = req.cookies;
+    const { adminCookies } = req.cookies;
 
     // 관리자인 경우
     if (adminCookies) {
@@ -36,16 +15,6 @@ router.get('/', async (req, res, next) => {
       err.statusCode = 403;
       return next(err);
     }
-
-    // 비밀번호 재확인 쿠키가 존재하지 않는 경우
-    if (!tempCookies) {
-      const err = new Error('비밀번호 재확인이 필요합니다.');
-      err.statusCode = 401;
-      return next(err);
-    }
-
-    // 비밀번호 재확인 토큰 확인
-    verifyToken(tempCookies, process.env.CONFIRM_JWT_SECRET_KEY);
 
     const user = await User.findById(res.locals.user._id).lean();
 
@@ -55,9 +24,6 @@ router.get('/', async (req, res, next) => {
       err.statusCode = 404;
       return next(err);
     }
-
-    // 비밀번호 재확인 쿠키 제거
-    res.clearCookie('tempCookies');
 
     // 4가지 user정보 반환(이름, 이메일, 주소, 전화번호)
     res.json({
@@ -99,6 +65,13 @@ router.put('/', async (req, res, next) => {
     }
 
     const user = await User.findById(res.locals.user._id).lean();
+
+    // 비밀번호 재확인 안 했을 경우
+    if (user.updateLock === true) {
+      const err = new Error('비밀번호를 재확인 해야 합니다.');
+      err.statusCode = 412;
+      return next(err);
+    }
 
     // 유저가 존재하지 않을 경우
     if (!user) {
@@ -165,10 +138,18 @@ router.put('/', async (req, res, next) => {
       password: hashPassword,
       phoneNumber,
       address: [postNumber, address, detailAddress],
+      updateLock: true,
     };
 
     // DB에 데이터 저장
-    await User.updateOne({ _id: res.locals.user._id }, data);
+    const updateData = await User.updateOne({ _id: res.locals.user._id }, data);
+
+    // 정상적으로 업데이트가 잘 됐는지 확인하는 코드
+    if (updateData.modifiedCount === 0) {
+      const err = new Error('사용자 정보를 업데이트 하는 과정에서 오류가 발생했습니다.');
+      err.statusCode = 400;
+      return next(err);
+    }
 
     // 수정한 데이터를 바탕으로 토큰 새로 만들어줌
     const newJsonToken = jwt.sign(
@@ -181,18 +162,15 @@ router.put('/', async (req, res, next) => {
       { expiresIn: '1h' },
     );
 
-    res
-      .status(201)
-      .cookie('userCookies', newJsonToken, { httpOnly: true, secure: true })
-      .json({
-        err: null,
-        data: {
-          name: data.name,
-          email: data.email,
-          address: data.address,
-          phoneNumber: data.phoneNumber,
-        },
-      });
+    res.cookie('userCookies', newJsonToken, { httpOnly: true }).json({
+      err: null,
+      data: {
+        name: data.name,
+        email: data.email,
+        address: data.address,
+        phoneNumber: data.phoneNumber,
+      },
+    });
   } catch (e) {
     next(e);
   }
@@ -220,7 +198,14 @@ router.put('/withdrawal', async (req, res, next) => {
       return next(err);
     }
 
-    await User.updateOne({ _id: user._id }, { isUser: false });
+    const updateData = await User.updateOne({ _id: user._id }, { isUser: false });
+    // 정상적으로 탈퇴됐는지 확인하는 코드
+    if (updateData.modifiedCount === 0) {
+      const err = new Error('탈퇴하는 과정에서 오류가 발생했습니다.');
+      err.statusCode = 400;
+      return next(err);
+    }
+
     res.clearCookie('userCookies');
     res.json({ err: null, data: { message: '탈퇴되었습니다.' } });
   } catch (e) {
