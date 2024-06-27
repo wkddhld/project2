e8236ee379e7af9d6a2b967d239d1dbb17889730
[IoT3] 조수빈 <cdw3521@naver.com>
2024-06-27 15:57:27 +0900

    Docs: 필요 없는 코드 삭제

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,13 +11,11 @@ const userOrderRouter = require('./userOrderRouter');
 const userDetailProductRouter = require('./userDetailProductRouter');
 const adminOrderRouter = require('./adminOrderRouter');
 const userInfoRouter = require('./userInfoRouter');
-const checkPasswordRouter = require('./checkPasswordRouter');
 const { isAuthenticatedMiddleware, isAuthenticatedAdminMiddleware } = require('../middlewares');
 
 apiRouter.use('/', authRouter); // 회원가입 및 로그인 라우터 연결
 apiRouter.use('/categories', categoryRouter); // 카테고리 조회 및 카테고리별 상품 조회하는 라우터 연결
 apiRouter.use('/admin/categories', isAuthenticatedAdminMiddleware, adminCategoryRouter); // 카테고리 수정 & 삭제 & 추가 라우터 연결
-apiRouter.use('/checkPassword', isAuthenticatedMiddleware, checkPasswordRouter); // 회원 정보 조회 & 수정 & 삭제 라우터 연결
 apiRouter.use('/my-page', isAuthenticatedMiddleware, userCrudRouter);
 apiRouter.use('/admin/subcategories', isAuthenticatedAdminMiddleware, adminSubCategoryRouter); // 소분류 카테고리 수정 & 삭제 & 추가 라우터 연결
 apiRouter.use('/admin/products', isAuthenticatedAdminMiddleware, adminProductRouter);
